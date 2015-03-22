23c33c5596b9c66135f30b9f1838c0a1512adb2d
Maciej Kumorek <mkumorek@gmail.com>
2015-03-22 17:33:22 +0000

    Moved Router class to its own file

---
+++ src/Conpanna/Route.cs
@@ -0,0 +1,17 @@
+﻿namespace Conpanna
+{
+    using System;
+
+    internal class Route
+    {
+        public Route(Method method, Action<Request, Response> handler)
+        {
+            Method = method;
+            Handler = handler;
+        }
+
+        public Method Method { get; private set; }
+
+        public Action<Request, Response> Handler { get; private set; }
+    }
+}
--- src/Conpanna/Router.cs
+++ src/Conpanna/Router.cs
@@ -45,20 +45,7 @@
                 select route.Handler;
         }
 
-        internal class Route
-        {
-            public Route(Method method, Action<Request, Response> handler)
-            {
-                Method = method;
-                Handler = handler;
-            }
-
-            public Method Method { get; private set; }
-
-            public Action<Request, Response> Handler { get; private set; }
-        }
-
-        internal class RouteHandlerCollection : List<Route>
+        private class RouteHandlerCollection : List<Route>
         {
         }
     }
